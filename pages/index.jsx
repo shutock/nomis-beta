8943bba8e8cafca3af36f826e47d527adbd66fd0
Denis Shutock <yoshutock@gmail.com>
2022-11-25 17:26:22 +0600

    Bug Fixes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Spline from "@splinetool/react-spline";
+// import Spline from "@splinetool/react-spline";
 
 import Layout from "../components/layout";
 
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="scene">
           {/* <Spline scene="https://prod.spline.design/1s7zQQpgAI6Jxzls/scene.splinecode" /> */}{" "}
           {/* Atom */}
-          <Spline scene="https://prod.spline.design/A-CAELRxlfHFcrWL/scene.splinecode" />
+          {/* <Spline scene="https://prod.spline.design/A-CAELRxlfHFcrWL/scene.splinecode" /> */}
         </div>
         <div className="wrapper">
           <h1>Headline</h1>
